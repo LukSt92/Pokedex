4e949ad54f665f26291d196261aa9697d858a410
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-03-31 23:17:30 +0200

    add more subpages add routes to new subpages

--- pokedex/src/main.jsx
+++ pokedex/src/main.jsx
@@ -4,6 +4,11 @@ import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./subpages/home/Home.jsx";
 import { Arena } from "./subpages/arena/Arena.jsx";
+import { Edit } from "./subpages/edit/Edit.jsx";
+import { Favourites } from "./subpages/favourites/Favourites.jsx";
+import { Login } from "./subpages/login/Login.jsx";
+import { Register } from "./subpages/register/Register.jsx";
+import { Ranking } from "./subpages/ranking/Ranking.jsx";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +17,11 @@ const router = createBrowserRouter([
     children: [
       { element: <Home />, path: "/" },
       { element: <Arena />, path: "/arena" },
+      { element: <Edit />, path: "/edit" },
+      { element: <Favourites />, path: "/favourites" },
+      { element: <Ranking />, path: "/ranking" },
+      { element: <Login />, path: "/login" },
+      { element: <Register />, path: "/register" },
     ],
   },
 ]);
--- pokedex/src/subpages/header/Header.jsx
+++ pokedex/src/subpages/header/Header.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { ThemeSwitch } from "./ThemeSwitch";
 
 const routes = [
-  { name: "Home", id: 1, path: "/" },
+  { name: "Favourites", id: 1, path: "favourites" },
   { name: "Arena", id: 2, path: "arena" },
+  { name: "Ranking", id: 3, path: "ranking" },
+  { name: "Edit", id: 4, path: "edit" },
+  { name: "Register", id: 5, path: "register" },
+  { name: "Login", id: 6, path: "login" },
 ];
 
 export const Header = () => {
   return (
     <div className="flex w-screen p-8 border-b-4 border-indigo-500">
-      <img src={"../../../International_Pokémon_logo.svg"}></img>
+      <Link to={"/"}>
+        <img src={"../../../International_Pokémon_logo.svg"}></img>
+      </Link>
       <div className="flex flex-col items-end grow gap-4">
         <div className="flex items-center gap-2">
           <h4>UserName TODO</h4>
@@ -18,14 +24,11 @@ export const Header = () => {
         </div>
         <div className="flex justify-stretch">
           {routes.map(({ name, id, path }) => (
-            <NavLink key={id} to={path}>
-              <button
-                type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              >
+            <Link key={id} to={path}>
+              <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">
                 {name}
               </button>
-            </NavLink>
+            </Link>
           ))}
         </div>
       </div>
