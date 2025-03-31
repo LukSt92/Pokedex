5dde527d9814c7e41b70e41bd3e207ac4f77bd45
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-03-31 13:52:22 +0200

    add: arena, header, home, layout components

---
+++ pokedex/src/subpages/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <img src="https://pl.wikipedia.org/wiki/Pok%C3%A9mon#/media/Plik:International_Pok%C3%A9mon_logo.svg" />
+      <div>
+        <div>
+          <h4>UserName TODO</h4>
+          <h4>Switch L/D</h4>
+        </div>
+        <div>
+          <button>Naw1</button>
+          <button>Nav2 etc.</button>
+        </div>
+      </div>
+    </div>
+  );
+};
