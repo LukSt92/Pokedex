f1136dc9d293409205ad9084c66092b0f2f91365
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-04-02 14:34:40 +0200

    add: db json add: axios add: createNewUser function in Register.jsx

--- pokedex/src/subpages/register/Register.jsx
+++ pokedex/src/subpages/register/Register.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Button } from "../../shared/Button";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import axios from "axios";
+
+const baseUrl = "http://localhost:3000/users";
 
 const inputClass =
   "block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer";
@@ -9,7 +12,6 @@ const labelClass =
   "peer-focus:font-medium absolute text-base text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6";
 
 export const Register = () => {
-  const [userData, setUserData] = useState();
   const {
     register,
     handleSubmit,
@@ -17,10 +19,24 @@ export const Register = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => setUserData(data);
+  const onSubmit = (data) => createNewUser(data);
 
-  //TODO ADD ZOD VALIDATION AND <p> FOR ERROR MESSAGES && LOOK INFO FOR NOTISTACK
-  // CREATE DBJSON TO STORE NEW USER DATA
+  async function createNewUser(data) {
+    axios
+      .post(baseUrl, {
+        userName: data.username,
+        email: data.email,
+        password: data.password,
+      })
+      .then(function (response) {
+        console.log(response);
+        //ADD NOTISTACK INFO HERE OR ADD FINALLY!!!
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <>
       <p className="text-4xl font-bold">Create new account</p>
