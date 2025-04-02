dfcfd99e28f0ef5cb86e6391641e877f14b909e5
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-04-02 21:51:52 +0200

    add: btn to log out

--- pokedex/src/subpages/header/Header.jsx
+++ pokedex/src/subpages/header/Header.jsx
@@ -1,18 +1,24 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { ThemeSwitch } from "./ThemeSwitch";
 import { Button } from "../../shared/Button";
+import { LoginContext } from "../../context/LoginContext";
 
-const routes = [
-  { name: "Favourites", id: 1, path: "favourites" },
-  { name: "Arena", id: 2, path: "arena" },
-  { name: "Ranking", id: 3, path: "ranking" },
-  { name: "Edit", id: 4, path: "edit" },
-  { name: "Register", id: 5, path: "register" },
-  { name: "Login", id: 6, path: "login" },
-];
-
+const routes = {
+  basics: [
+    { name: "Favourites", id: 1, path: "favourites" },
+    { name: "Arena", id: 2, path: "arena" },
+    { name: "Ranking", id: 3, path: "ranking" },
+    { name: "Edit", id: 4, path: "edit" },
+  ],
+  toLog: [
+    { name: "Register", id: 5, path: "register" },
+    { name: "Login", id: 6, path: "login" },
+  ],
+};
 export const Header = () => {
+  const { isLoggedIn, setLoggedIn } = useContext(LoginContext);
+
   return (
     <div className="flex w-screen p-8 border-b-4 border-indigo-500">
       <Link to={"/"} className="shrink-0">
@@ -24,11 +30,20 @@ export const Header = () => {
           <ThemeSwitch />
         </div>
         <div className="flex flex-wrap">
-          {routes.map(({ name, id, path }) => (
+          {routes.basics.map(({ name, id, path }) => (
             <Link key={id} to={path}>
               <Button>{name}</Button>
             </Link>
           ))}
+          {!isLoggedIn ? (
+            routes.toLog.map(({ name, id, path }) => (
+              <Link key={id} to={path}>
+                <Button>{name}</Button>
+              </Link>
+            ))
+          ) : (
+            <Button onClick={() => setLoggedIn(false)}>Log out</Button>
+          )}
         </div>
       </div>
     </div>
